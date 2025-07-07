682f5f656a28cc0cd23303acf1d3940b9247c223
Srikanth M <srikanth.m06@ad.infosys.com>
2025-07-07 15:52:44 +0530

    predictor.py script debugging

--- src/predictor.py
+++ src/predictor.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(file), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
  
 import pickle
 import pandas as pd

1b4869f33bd09a63ed611f90f62b94ca81d546fe
Srikanth M <srikanth.m06@ad.infosys.com>
2025-07-07 15:50:22 +0530

    src folder indentation issue

--- src/predictor.py
+++ src/predictor.py
@@ -6,8 +6,8 @@ import pickle
 import pandas as pd
  
 def load_model(path):
-with open(path, "rb") as f:
-return pickle.load(f)
+    with open(path, "rb") as f:
+        return pickle.load(f)
  
 def predict(model, X: pd.DataFrame):
-return model.predict(X)
+    return model.predict(X)
